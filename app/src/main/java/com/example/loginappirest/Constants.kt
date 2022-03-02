a226789a45023e5103edd88965a858d288e4609b
Antonio <antoniogallegos086@gmail.com>
2022-03-01 23:30:49 -0600

    UI interface

---
+++ app/src/main/java/com/example/loginappirest/Constants.kt
@@ -0,0 +1,18 @@
+package com.example.loginappirest
+
+object Constants {
+    const val BASE_URL = "https://reqres.in/"
+
+    const val API_Path = "/api"
+    const val API_REGISTER = "/register"
+    const val API_LOGIN = "login"
+
+    const val EMAIL_PARAM = "email"
+    const val PASSWORD_PARA = "password"
+
+
+    const val ID_PROPERTY = "id"
+    const val TOKEN_PROPERTY = "token"
+
+    const val ERROR_VALUE = "fail"
+}
--- app/src/main/java/com/example/loginappirest/ReqResApi.kt
+++ app/src/main/java/com/example/loginappirest/ReqResApi.kt
@@ -5,17 +5,7 @@ import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.Volley
 
-/****
- * Project: Stores
- * From: com.cursosant.android.stores.common.database
- * Created by Alain Nicolás Tello on 2/6/21 at 10:26 AM
- * Course: Android Practical with Kotlin from zero.
- * Only on: https://www.udemy.com/course/kotlin-intensivo/
- * All rights reserved 2021.
- *
- * All my Courses(Only on Udemy):
- * https://www.udemy.com/user/alain-nicolas-tello/
- ***/
+
 class ReqResApi constructor(context: Context) {
     companion object{
         @Volatile
