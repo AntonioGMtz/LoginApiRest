a2c0331dade6c7a9a5f2b10268bb07af7993a7f9
Antonio <antoniogallegos086@gmail.com>
2022-03-03 00:12:03 -0600

    JSON

--- app/src/main/java/com/example/loginappirest/MainActivity.kt
+++ app/src/main/java/com/example/loginappirest/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.loginappirest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.loginappirest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mBinding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        mBinding.swType.setOnCheckedChangeListener{ button ,checked ->
+            button.text = if(checked) getString(R.string.main_type_login)
+            else getString(R.string.main_type_register)
+            mBinding.swType.text = button.text
+        }
+
+        mBinding.btnLogin.setOnClickListener{
+            login()
+        }
+    }
+
+    private fun login() {
+        updateUI(":)")
+    }
+
+    private fun updateUI(result: String) {
+        mBinding.tvResult.visibility = View.VISIBLE
+        mBinding.tvResult.text= result
     }
 }
